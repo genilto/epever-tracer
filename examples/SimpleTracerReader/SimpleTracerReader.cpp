74421630c81b30048fbb3a071c328a8c8359db0a
Genilto Vanzin <genilto.vanzin@gmail.com>
2022-07-06 17:26:53 -0300

    First release! 0.0.1

--- examples/SimpleTracerReader/SimpleTracerReader.cpp
+++ examples/SimpleTracerReader/SimpleTracerReader.cpp
@@ -12,7 +12,7 @@ void setup()
     Serial.begin(115200);
 
     // Init the readings of rs485 from Tracer
-    // tracer.begin();
+    tracer.begin();
 
     DebugPrintln("Setup OK!");
     DebugPrintln("----------------------------");
@@ -21,13 +21,16 @@ void setup()
 
 void loop()
 {   
-    // if (!tracer.update()) {
-    //     DebugPrintln("Error reading Tracer");
-    // } else {
-    //     TracerData *data = tracer.getData();
-    //     DebugPrint( "Battery Voltage: " );
-    //     DebugPrintln( data->realtimeData.batteryVoltage );
-    // }
+    if (!tracer.update()) {
+        DebugPrintln("Error reading Tracer");
+    } else {
+        TracerData *data = tracer.getData();
+        
+        if (data->everythingRead) {
+            DebugPrint( "Battery Voltage: " );
+            DebugPrintln( data->realtimeData.batteryVoltage );
+        }
+    }
     
     delay(1000);
 }
